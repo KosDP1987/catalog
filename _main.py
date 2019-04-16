42b0f8ee04eeecd17659081de7d25f4fe4192cf5
Bazilevskiy Konstantin <you@example.com>
2019-04-17 00:25:12 +0300

    first commit

---
+++ _main.py
@@ -0,0 +1,21 @@
+# основной файл - реализация поиска, в зависимости от поиска обращение производиться к классу(предприятие или персон)
+# тут же создание меню и обработчика ошибок ввода
+from Personal import Person
+#m = open('E:\MyDocuments_23032019\Pyton\catalog\menu.txt','r')
+#data = m.readlines() # read all lines
+#print(data)
+#m.close()
+
+Menu1 ="""
+                #----------- Menu ---------#
+
+                #   1.  #
+                #   2.           # 
+                #   0.                    #
+
+                #-------------------------------#
+\n
+"""
+
+print (Menu1)
+print("Menu2")
